32c24eb53a909efda45f9dd102d12517035b0cf9
agafonova14011985 <agafonova14011985@mail.ru>
2022-01-25 23:39:13 +0700

    client-chat-1

--- chat-client/src/main/java/january_chat/chat_client/MainChatController.java
+++ chat-client/src/main/java/january_chat/chat_client/MainChatController.java
@@ -52,20 +52,30 @@ public class MainChatController implements Initializable {
         if (message.isBlank()){
             return;//ни чего не происходит
         }
-        //но если есть что то
-        mainChatArea.appendText(message + System.lineSeparator());
+
+        //но если есть что то, он не пустой то дастаем из контакт листа ListView, через
+        //getSelectionModel() достаем выбранный объект
+        //далее добавляем этот объект recipient + а так же сообщение
+               var recipient = contactList.getSelectionModel().getSelectedItem();
+        mainChatArea.appendText(recipient + ":\n" + message  + System.lineSeparator());
         //и очистим поле ввода после ввода
         inputField.clear();
+
+
     }
 
-    @Override
+    @Override     //выполняется на старте контроллера, заполняется..
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //на старте приложения вложем фейковый список контактов
         var contacts = new ArrayList<String>();
+        contacts.add("ALL:");    //выбор всех контактов
         for (int i = 0; i < 10; i ++) {
             contacts.add("Контакт#" + (i + 1) );
         }
         //добавим список в
         contactList.setItems(FXCollections.observableList(contacts));
+        //выбор из списка
+        contactList.getSelectionModel().selectFirst();
+        //contactList.getSelectionModel().
     }
 }
