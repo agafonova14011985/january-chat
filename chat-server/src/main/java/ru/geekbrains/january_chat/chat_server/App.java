31b349876657224a1e92d5baa9c1e4c34bf76415
agafonova14011985 <agafonova14011985@mail.ru>
2022-02-11 00:52:00 +0700

    chat lesson 8 home4

--- chat-server/src/main/java/ru/geekbrains/january_chat/chat_server/App.java
+++ chat-server/src/main/java/ru/geekbrains/january_chat/chat_server/App.java
@@ -6,6 +6,8 @@ import ru.geekbrains.january_chat.chat_server.server.Server;
 public class App {
     public static void main(String[] args) {
         new Server(new InMemoryAuthService()).start();
+
+
     }
 }
 
