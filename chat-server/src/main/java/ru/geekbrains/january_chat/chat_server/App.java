126f85d0a8094a0f865d615637a98bfe96323ab8
agafonova14011985 <agafonova14011985@mail.ru>
2022-02-19 19:06:26 +0700

    chat lesson2 Database HomeWork

--- chat-server/src/main/java/ru/geekbrains/january_chat/chat_server/App.java
+++ chat-server/src/main/java/ru/geekbrains/january_chat/chat_server/App.java
@@ -1,11 +1,12 @@
 package ru.geekbrains.january_chat.chat_server;
 
+import ru.geekbrains.january_chat.chat_server.auth.DatabaseAuthService;
 import ru.geekbrains.january_chat.chat_server.auth.InMemoryAuthService;
 import ru.geekbrains.january_chat.chat_server.server.Server;
-
+//создаем экземпляр сервера и передаем ему тип DatabaseAuthService
 public class App {
     public static void main(String[] args) {
-        new Server(new InMemoryAuthService()).start();
+        new Server(new DatabaseAuthService()).start();
 
 
     }
